a310e6cbe84968e3a58f95ec0cb97c106e15e80f
DaniDhax <DaniDhax@users.noreply.github.com>
2023-08-31 11:18:10 +0200

    Añadida página de plantillas de colores de fondo para shorts de Vamos al Lío

---
+++ app.js
@@ -0,0 +1,5 @@
+const $btnCargarPlantillas = document.getElementById("btn-cargar-plantillas");
+
+$btnCargarPlantillas.addEventListener("click", () => {
+  window.location.href = "pages/page1.html";
+});
